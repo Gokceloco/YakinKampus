abe17e5027384fce2bc636b072b09401f865b7d5
gokcegun <gokce@locomotive.games>
2024-09-13 11:08:26 +0300

    Ders3

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Box : MonoBehaviour
 {
-    float speed = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,29 +13,6 @@ public class Box : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            speed = 5;
-        }
-        else
-        {
-            speed = 2;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position = transform.position + Vector3.forward * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position = transform.position + Vector3.back * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position = transform.position + Vector3.left * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.position = transform.position + Vector3.right * speed * Time.deltaTime;
-        }        
+        transform.position = transform.position + Vector3.up * .1f;      
     }
 }
